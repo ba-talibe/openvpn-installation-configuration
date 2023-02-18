625aa5b9b064722324d644ceb8ac38b878145286
ba-talibe <omarba2015@gmail.com>
2023-02-18 16:38:09 +0000

    rcoorection copy error

--- create_client_cert.sh
+++ create_client_cert.sh
@@ -1,4 +1,3 @@
-#! /usr/bin/bash
 export P="0"
 
 if [ "$1" == '' ] ; then
@@ -7,6 +6,8 @@ fi
 if [ "$P" -eq "1" ];then
 	echo "syntax :"
     echo "        ./create_client_cert.sh [client_name]"
+    echo "to create zip archive for windows client"
+    echo "        ./create_client_cert.sh [client_name] win"
     echo "make sure the script is executable :-)"
     echo "happy hacks"
     exit
@@ -14,8 +15,10 @@ fi
 
 
 mkdir $1
-EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full $1 nopass || echo "An error occurs" && exit
-cp pki/ca.crt  pki/private/ca.key pki/private/$1.key  pki/issued/$1.crt pki/dh.pem pki/crl.pem  pki/tc.key $1/
-tar -cvf $1.tar $1/
-zip $1/ 
-echo "A zip archive file is generated for windows clients"
+EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full $1 nopass && \
+cp pki/ca.crt  pki/private/ca.key pki/private/$1.key  pki/issued/$1.crt pki/dh.pem pki/crl.pem  pki/tc.key $1/ && \
+if [ "$2" == "" ]
+    tar -cvf $1.tar $1/ || echo "An error occurs while generating PEM et CRT files" && exit
+else
+    zip $1/
+fi
