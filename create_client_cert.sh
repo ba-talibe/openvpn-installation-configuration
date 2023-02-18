01be3de738d799851504e3043065f32cd4887538
ba-talibe <omarba2015@gmail.com>
2023-02-18 14:19:44 +0000

    handle missing parameters

--- create_client_cert.sh
+++ create_client_cert.sh
@@ -1,6 +1,21 @@
 #! /usr/bin/bash
+export P="0"
+
+if [ "$1" == '' ] ; then
+	export P="1"
+fi
+if [ "$P" -eq "1" ];then
+	echo "syntax :"
+    echo "        ./create_client_cert.sh [client_name]"
+    echo "make sure the script is executable :-)"
+    echo "happy hacks"
+    exit
+fi
+
 
 mkdir $1
 EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full $1 nopass || echo "An error occurs" && exit
 cp pki/ca.crt  pki/private/ca.key pki/private/$1.key  pki/issued/$1.crt pki/dh.pem pki/crl.pem  pki/tc.key $1/
 tar -cvf $1.tar $1/
+zip $1/ 
+echo "A zip archive file is generated for windows clients"
